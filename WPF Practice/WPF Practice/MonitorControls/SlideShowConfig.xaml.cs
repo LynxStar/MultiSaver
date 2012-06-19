60e07c064a049470e7d2f2c06382ed23c11030b2
Timothy Brantley <tbrantleyii@gmail.com>
2012-06-19 08:04:17 -0400

    finishing SlideShow Config

--- WPF Practice/WPF Practice/MonitorControls/SlideShowConfig.xaml.cs
+++ WPF Practice/WPF Practice/MonitorControls/SlideShowConfig.xaml.cs
@@ -19,9 +19,46 @@ namespace WPF_Practice.MonitorControls
     /// </summary>
     public partial class SlideShowConfig : UserControl
     {
+        public int FadeTime
+        {
+            get { return Int32.Parse(txtFadeTime.Text); } 
+            set { txtFadeTime.Text = value.ToString(); }
+        }
+        public int DisplayTime
+        {
+            get{return Int32.Parse(txtDisplayTime.Text);}
+            set { txtDisplayTime.Text = value.ToString(); }
+        }
+        public int PanTime
+        {
+            get {return Int32.Parse(txtPanTime.Text);}
+            set { txtPanTime.Text = value.ToString(); }
+        }
+        public int Rotation
+        {
+            get{return Int32.Parse(txtRotation.Text);}
+            set { txtRotation.Text = value.ToString(); }
+        }
+        public bool Clockwise
+        {
+            get { return (bool)isClockwise.IsChecked; }
+            set { isClockwise.IsChecked = value; }
+        }
+        public bool isAlphabetical
+        {
+            get
+            {
+                if (radioA.IsChecked)
+                    return true;
+                else if(radioRevA)
+                    return false;
+                }
+        }
         public SlideShowConfig()
         {
             InitializeComponent();
         }
+
+        
     }
 }
