02876aff190fa0f621d8d6081f43ad677ad49d5b
Action Jackson <jacksona1@wit.edu>
2012-07-10 12:52:59 -0400

    Data hierarchy and beginning of XML handler

---
+++ MultiSaver/ConfigPanel/GroupSetting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfigPanel
+{
+    public class GroupSetting
+    {
+
+
+        public string groupName;
+        public List<MonitorSetting> monitors;
+
+        public string ssType;
+        public bool isActive;
+        public string albumLocation;
+        public string order;
+        public int mazeSize;
+        public string mazePalletName;
+
+
+    }
+
+    class MonitorSetting
+    {
+        public int monitorId;
+        public int fadeTime;
+        public int displayTime;
+        int panTime;
+        public int dirIn;
+        public int dirOut;
+        public bool clockwise;
+        public int numRotations;
+
+        public string aiView;
+        public string aiMethod;
+
+
+    }
+}
---
+++ MultiSaver/ConfigPanel/XMLHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml
+namespace ConfigPanel
+{
+    class XMLHandler
+    {
+        public static void save(List<GroupSetting> groups)
+        {
+            XmlWriter writer = XmlWriter.Create("config.xml");
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Groups");
+            foreach(GroupSetting gs in groups)
+            {
+                writer.WriteStartElement("Group");
+                writer.WriteElementString("Name", gs.groupName);
+                writer.WriteElementString("Type", gs.ssType);
+                writer.WriteElementString("Active?", gs.isActive.ToString());
+                writer.WriteElementString("Album", gs.albumLocation);
+                writer.WriteElementString("PictureOrder", gs.order);
+                writer.WriteElementString("MazeSize", gs.mazeSize.ToString());
+                writer.WriteElementString("Pallet", gs.mazePalletName);
+                foreach(MonitorSetting ms in gs.monitors)
+                {
+                    writer.WriteStartElement("Monitor");
+                    writer.WriteElementString("ID", ms.monitorId.ToString());
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+            }
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+        }
+
+        public static List<GroupSetting> load(string pathname)
+        {
+
+        }
+    }
+}
